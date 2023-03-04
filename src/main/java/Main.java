3d50f6c1f8e448bf8af0a49fed76ff706ad29fdb
Luiz Roberto <luizrobertoao29@gmail.com>
2023-03-04 18:56:59 -0300

    Inicio da criação das classes Mentoria e Curso.

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição curso Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1 + "\n" + curso2 + "\n" + mentoria);
+    }
+}
